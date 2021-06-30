445fd32559c8636b85156427a5a05d0268d87121
Gaby Tal <45428645+gabytal@users.noreply.github.com>
2021-06-30 18:40:32 +0300

    Create HTTPToMultipleHosts.py

---
+++ HTTPToMultipleHosts.py
@@ -0,0 +1,34 @@
+import requests
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+grids = {"dev": {1: 'host-1.dev.lan',
+                 2: 'host-2.dev.lan'},
+         "qa": {1: 'host-1.qa.lan',
+                2: 'host-2.qa.lan'},
+         "stg": {1: 'host-1.stg.lan',
+                 2: 'host-2.stg.lan'},
+         "uk": {1: 'host-1.uk.lan',
+                2: 'host-2.uk.lan'},
+         "us": {1: 'host-1.us.lan',
+                2: 'host-2.us.lan'}
+                }
+
+for grid in grids.keys():
+    print(f"grid: {grid}")
+    for host in grids.get(grid):
+        try:
+            r = requests.get(url=f"https://{grids[grid][host]}/v2/flow_test",
+                             headers={},
+                             files={},
+                             auth=None,
+                             verify=False)
+            if r.status_code != 200:
+                print(f'BAD Response:', grid, host, r.status_code)
+            else:
+                print(f"Host: {grids[grid][host].split('.')[0]} OK!")
+        except Exception as e:
+            print(e)
+
+print("Done")
